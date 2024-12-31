6c7386f2ae7f9d7dc07884118497b7f380dbcb05
michallote_linux <michel.gordillo.a@comunidad.unam.mx>
2024-12-31 14:57:51 -0600

    added docstring to units

--- src/utils/units.py
+++ src/utils/units.py
@@ -1,4 +1,53 @@
 class Units:
+    """
+    A utility class for unit conversions to SI units.
+
+    Attributes
+    ----------
+    mm : float
+        Millimeters to meters conversion factor (1e-3).
+    cm : float
+        Centimeters to meters conversion factor (1e-2).
+    m : float
+        Meters to meters conversion factor (1).
+    km : float
+        Kilometers to meters conversion factor (1e3).
+    inch : float
+        Inches to meters conversion factor (0.0254).
+    ft : float
+        Feet to meters conversion factor (0.3048).
+    yd : float
+        Yards to meters conversion factor (0.9144).
+    mile : float
+        Miles to meters conversion factor (1609.34).
+    mils : float
+        Mils to meters conversion factor (0.0254 / 1000).
+
+    g : float
+        Grams to kilograms conversion factor (1e-3).
+    kg : float
+        Kilograms to kilograms conversion factor (1).
+    tonne : float
+        Tonnes to kilograms conversion factor (1e3).
+    lb : float
+        Pounds to kilograms conversion factor (0.453592).
+    oz : float
+        Ounces to kilograms conversion factor (0.0283495).
+
+    s : float
+        Seconds to seconds conversion factor (1).
+    min : float
+        Minutes to seconds conversion factor (60).
+    hr : float
+        Hours to seconds conversion factor (3600).
+    day : float
+        Days to seconds conversion factor (86400).
+
+    Methods
+    to_SI(value, unit)
+        Converts a given value from the specified unit to its SI equivalent.
+    """
+
     __slots__ = ()
     # Length units (meters)
     mm = 1e-3
@@ -25,7 +74,21 @@ class Units:
     day = 86400
 
     @staticmethod
-    def to_SI(value, unit):
+    def to_SI(value: float, unit: str) -> float:
+        """Converts a given value from the specified unit to its SI equivalent.
+
+        Parameters
+        ----------
+        value : float
+            Value to convert.
+        unit : str
+            Unit to convert from.
+
+        Returns
+        -------
+        float
+            Value in SI units.
+        """
         return value * getattr(Units, unit, 1)
 
 
